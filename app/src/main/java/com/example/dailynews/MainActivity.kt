fc6ad3da5e192bf0ca1ab93d0669133f38c95fb2
Aline Ferreira <alinfst@gmail.com>
2024-07-04 11:54:44 -0300

    add composable to News Screen

--- app/src/main/java/com/example/dailynews/MainActivity.kt
+++ app/src/main/java/com/example/dailynews/MainActivity.kt
@@ -4,7 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.dailynews.ui.theme.BlackTextColor
 import com.example.dailynews.ui.theme.DailyNewsTheme
+import com.example.dailynews.ui.theme.GrayTextColor
+import com.example.dailynews.ui.theme.White
+
+data class Suggestion(val title: String = "", var isFocused: Boolean = false)
+object SuggestionList {
+    val list: List<Suggestion> = listOf(
+        Suggestion("All"),
+        Suggestion("Politic"),
+        Suggestion("Sport"),
+        Suggestion("Education"),
+        Suggestion("Economy"),
+        Suggestion("Technology")
+    )
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +45,46 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun NewsScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+    ) {
+        Header()
+    }
+}
+
+@Composable
+fun Header(modifier: Modifier = Modifier) {
+    Text(
+        text = "Discover",
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        color = BlackTextColor
+    )
+    Spacer(modifier = Modifier.height(4.dp))
+    Text(
+        text = "News from all aroun the world",
+        fontSize = 14.sp,
+        color = GrayTextColor
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun NewsScreenPreview() {
+    DailyNewsTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = White
+        ) {
+            NewsScreen()
+        }
+    }
+}
+
 
 
