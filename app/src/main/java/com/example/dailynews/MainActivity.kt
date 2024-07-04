8cb03c6f05537c226d52a9993581aca41be83fef
Aline Ferreira <alinfst@gmail.com>
2024-07-04 12:18:09 -0300

    add searchbar in News Screen

--- app/src/main/java/com/example/dailynews/MainActivity.kt
+++ app/src/main/java/com/example/dailynews/MainActivity.kt
@@ -4,21 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.dailynews.ui.theme.BlackTextColor
 import com.example.dailynews.ui.theme.DailyNewsTheme
+import com.example.dailynews.ui.theme.Gray
 import com.example.dailynews.ui.theme.GrayTextColor
 import com.example.dailynews.ui.theme.White
 
@@ -54,6 +71,8 @@ fun NewsScreen(modifier: Modifier = Modifier) {
             .padding(start = 16.dp, end = 16.dp, top = 16.dp)
     ) {
         Header()
+        Spacer(modifier = modifier.height(15.dp))
+        SearchBar()
     }
 }
 
@@ -65,12 +84,46 @@ fun Header(modifier: Modifier = Modifier) {
         fontWeight = FontWeight.Bold,
         color = BlackTextColor
     )
-    Spacer(modifier = Modifier.height(4.dp))
+    Spacer(modifier = Modifier.height(8.dp))
     Text(
         text = "News from all aroun the world",
         fontSize = 14.sp,
         color = GrayTextColor
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Row(
+        modifier
+            .fillMaxWidth()
+            .background(color = Gray)
+            .clip(shape = RoundedCornerShape(10.dp))
+    ) {
+        OutlinedTextField(value = text,
+            onValueChange = {
+                text = it
+            },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = White,
+                unfocusedBorderColor = White
+            ),
+            modifier = modifier.weight(1f),
+            placeholder = {
+                Row {
+                    Icon(imageVector = Icons.Filled.Search,
+                        contentDescription = "Search Icon"
+                    )
+                    Spacer(modifier = modifier.width(10.dp))
+                    Text(text = "Search")
+                }
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
